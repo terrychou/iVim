701ceb2d4c3413aedba1f0fc295acd30c33f3f38
Terry Chou <terrychoucn@gmail.com>
2017-07-20 15:32:38 +0800

    Initial Commit

---
+++ iVim/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iVim
+//
+//  Created by Terry on 7/20/17.
+//  Copyright © 2017 Boogaloo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
