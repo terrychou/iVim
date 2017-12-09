8171ee8595faa4444495d88a2fd3f7008e5fc98a
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2017-12-09 22:20:10 +0100

    Edited typo

--- get_frameworks.sh
+++ get_frameworks.sh
@@ -4,7 +4,7 @@ HHROOT="https://github.com/holzschu"
 
 (cd "${BASH_SOURCE%/*}/Frameworks"
 # ios_system
-echo "Downloading ios_system.framework.zip"
+echo "Downloading frameworks"
 curl -OL $HHROOT/iVim/releases/download/v0.1/frameworks.tar.gz
 ( tar xvzf frameworks.tar.gz && rm frameworks.tar.gz ) || { echo "ios_system failed to download"; exit 1; }
 )
