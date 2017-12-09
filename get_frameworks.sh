1e833cdbcfbd7799d39151ab13114c8542b93db2
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2017-12-09 22:49:41 +0100

    Edited get_frameworks to get Python+Lua includes

--- get_frameworks.sh
+++ get_frameworks.sh
@@ -9,3 +9,17 @@ curl -OL $HHROOT/iVim/releases/download/v0.1/frameworks.tar.gz
 ( tar xvzf frameworks.tar.gz && rm frameworks.tar.gz ) || { echo "ios_system failed to download"; exit 1; }
 )
 
+# Optional: get also sources for Python and Lua:
+( # Python_ios
+cd "${BASH_SOURCE%/*}/.."
+git clone https://github.com/holzschu/python_ios
+cd "python_ios"
+sh ./getPackages.sh
+)
+( # lua_ios 
+cd "${BASH_SOURCE%/*}/.."
+git clone https://github.com/holzschu/lua_ios
+cd "lua_ios"
+sh ./get_lua_source.sh
+)
+
